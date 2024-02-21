44a8f90097a6cd550ceaffe2e19e734535766acd
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-02-21 17:49:39 +0600

    push

--- app/page.js
+++ app/page.js
@@ -1,5 +1,20 @@
 import Image from "next/image";
 
 export default function Home() {
-    return <main></main>;
+    return (
+        <main>
+            <section>
+                <div className="container mx-auto px-2 h-[100vh]">
+                    <div>
+                        <h1 className="text-5xl text-white font-bold">
+                            Business IT Solutions:
+                            <br /> <span className="text-[]">Pioneering Excellence for</span>
+                            Your Technological Needs
+                        </h1>
+                    </div>
+                    <div></div>
+                </div>
+            </section>
+        </main>
+    );
 }
