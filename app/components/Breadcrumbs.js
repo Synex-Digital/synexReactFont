7cdc28bf7180238c2ce140f097eb1bf5613cd3f6
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-03-13 19:30:45 +0600

    icon responsive

--- app/components/Breadcrumbs.js
+++ app/components/Breadcrumbs.js
@@ -26,58 +26,58 @@ const Breadcrumbs = ({ title, stateoff }) => {
     return (
         <div>
             <div id="banner" className="!relative container mx-auto">
-                <div className="round1 absolute rounded-full text-primary top-[128px] right-[43px] text-4xl">
+                <div className="round1 absolute rounded-full text-primary top-[128px] right-[43px] xl:text-4xl md:text-3xl text-xl">
                     <FaLaravel />
                 </div>
-                <div className="cross1 absolute text-primary text-4xl top-[80px] right-[400px]">
+                <div className="cross1 absolute text-primary xl:text-4xl md:text-3xl text-xl sm:top-[80px] top-20 sm:right-[400px] right-3/4">
                     <FaReact />
                 </div>
-                <div className="round3 absolute rounded-full text-primary top-[120px] sm:left-[400px] left-20 text-4xl">
+                <div className="round3 absolute rounded-full text-primary top-[120px] sm:left-[400px] right-[300px] xl:text-4xl md:text-3xl text-xl">
                     <FaNode />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[20px] left-[300px] text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[20px] sm:left-[300px] left-20 xl:text-4xl md:text-3xl text-xl">
                     <FaAws />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[20px] right-[40px] text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[20px] right-[40px] xl:text-4xl md:text-3xl text-xl">
                     <SiFirebase />
                 </div>
-                <div className="round1 absolute rounded-full text-primary top-[188px] right-[150px] text-4xl">
+                <div className="round1 absolute rounded-full text-primary top-[188px] right-[150px] xl:text-4xl md:text-3xl text-xl">
                     <SiPostgresql />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[200px] sm:left-[350px] left-16 text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[200px] sm:left-[350px] left-16 xl:text-4xl md:text-3xl text-xl">
                     <SiMysql />
                 </div>
-                <div className="round1 absolute rounded-full text-primary top-[200px] right-[400px] text-4xl">
+                <div className="round1 absolute rounded-full text-primary top-[200px] sm:right-[400px] left-[300px] xl:text-4xl md:text-3xl text-xl">
                     <SiMongodb />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[250px] left-[100px] text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[250px] left-[100px] xl:text-4xl md:text-3xl text-xl">
                     <FaPhp />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[150px] left-[100px] text-3xl">
+                <div className="round2 absolute rounded-full text-primary top-[150px] left-[100px] xl:text-3xl md:text-2xl text-lg">
                     <SiJavascript />
                 </div>
-                <div className="cross1 absolute rounded-full text-primary top-[70px] left-1/2 text-4xl">
+                <div className="cross1 absolute rounded-full text-primary top-[70px] left-1/2 xl:text-4xl md:text-3xl text-xl">
                     <TbBrandNextjs />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[0px] left-20 text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[0px] left-20 xl:text-4xl md:text-3xl text-xl">
                     <TbSeo />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[20px] left-1/2 text-3xl">
+                <div className="round2 absolute rounded-full text-primary top-[20px] left-1/2 xl:text-3xl md:text-2xl text-lg">
                     <FaFigma />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[40px] left-1/3 text-3xl">
+                <div className="round2 absolute rounded-full text-primary top-[40px] left-1/3 xl:text-3xl md:text-2xl text-lg">
                     <SiFlutter />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[250px] left-3/4 text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[250px] left-3/4 xl:text-4xl md:text-3xl text-xl">
                     <FaPython />
                 </div>
-                <div className="round2 absolute rounded-full text-primary top-[40px] left-3/4 text-4xl">
+                <div className="round2 absolute rounded-full text-primary top-[40px] left-3/4 xl:text-4xl md:text-3xl text-xl">
                     <FaVuejs />
                 </div>
                 <div className="cross2"></div>
             </div>
             <div className=" py-36">
-                <h2 className="text-white lg:text-5xl md:text-4xl sm:text-3xl text-xl   font-semibold text-center">
+                <h2 className="text-white lg:text-5xl md:text-4xl text-3xl font-semibold text-center">
                     {title}
                 </h2>
                 {stateoff && (
